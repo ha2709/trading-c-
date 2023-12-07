1d36bc8f7c3d3e033c400ee7a55a691e291d0308
“Ha <ngpha@yahoo.com>
2023-12-07 17:04:46 +0700

    hello world

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+    double principal = 0.0;
+    const int monthsInYear = 12;
+    cout << "Enter the pricipal amount: ";
+    cin >> principal;
+    double interest = 0.05;
+    int yearsOfLoan = 30;
+
+    double monthInterest = interest/monthsInYear;
+    long monthOfLoan = yearsOfLoan * monthsInYear;
+
+    cout << principal << " " <<interest << " " << yearsOfLoan << " " << monthInterest << endl;
+
+
+
+    return 0;
+}
