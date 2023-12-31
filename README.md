ae13cdf8bd7eb467963281954537455f6a8c2465
“Ha <ngpha@yahoo.com>
2023-12-31 21:58:03 +0700

    function for divide and multiplier

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Compile the code : `g++ -o trading trading.cpp`
 
 run the code `./trading`
+
+g++ -o hello hello.cpp
--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,38 @@
 # include <iostream>
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World" << std::endl;
+double divisor(double numerator, double denominator)
+{
+    return numerator/denominator;
+}
+
+double multiplier(double fristNum, double secondNum)
+{
+    return fristNum * secondNum;
+}
+
+int main() 
+{
+    // std::cout << "Hello, World" << std::endl;
     double principal = 0.0;
     const int monthsInYear = 12;
+    const int percentDenominator =100;
     cout << "Enter the pricipal amount: ";
     cin >> principal;
-    double interest = 0.05;
-    int yearsOfLoan = 30;
+ 
 
-    double monthInterest = interest/monthsInYear;
-    long monthOfLoan = yearsOfLoan * monthsInYear;
-
-    cout << principal << " " <<interest << " " << yearsOfLoan << " " << monthInterest << endl;
+    cout << "Eneter the principal amount: ";
+    cin >> principal;
+    double humanInterest = 0.0;
+    cout << "Enter the interest rate: ";
+    cin >> humanInterest;
+    double interest = divisor(humanInterest,percentDenominator);
+    int yearsOfLoan = 0;
+    cout << "Enter the years of loan:";
+    cin >> yearsOfLoan;
+    double monthInterest = divisor(interest,monthsInYear);
+    long monthOfLoan = multiplier( yearsOfLoan , monthsInYear);
+    cout << principal << " " << interest << " " << yearsOfLoan << " " << monthInterest << " " << monthOfLoan << endl;
 
     return 0;
 }
